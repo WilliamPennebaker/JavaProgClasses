f1f02c81dee220ff84ac13ca8704a698cf4acb91
William Pennebaker <42590061+WilliamPennebaker@users.noreply.github.com>
2022-09-30 11:58:47 -0400

    Added comments going over if statements and loops/arrays.

--- src/Programming1/IfStatements.java
+++ src/Programming1/IfStatements.java
@@ -8,41 +8,53 @@ package Programming1;
 public class IfStatements {
     public static void main(String[] args) {
         /**
-         * 
+         * Defining and declaring our num variable to 10.
          */
         int num = 10;
         
         /**
-         * 
+         * The first if statement. If our num variable is equal to (==) 10 then we print "The number 10."
          */
         if(num == 10) {
             System.out.println("The number 10.");
         }
         
         /**
-         * 
+         * If else statement. If num is equal to 10 then we print "The number 10". If num is anything "else" then we print that num.
          */
-        if(num == 11) {
+        if(num == 10) {
             System.out.println("The number 11.");
         } else {
-            System.out.println("The number 10.");
+            System.out.println("The number " + num + ".");
         }
         
         /**
-         * 
+         * If else chain statement.
+         * If num equals 10, print "The number 10."
+         * Else if num equals 11, print "The number 11."
+         * For the else statement we are just printing "Option not handled in this if statement."- This is an example of error handling which will
+         * be discussed later.
          */
-        if(num == 11) {
-            System.out.println("The number 11.");
-        } else if(num == 10) {
+        if(num == 10) {
             System.out.println("The number 10.");
+        } else if(num == 11) {
+            System.out.println("The number 11.");
         } else {
             System.out.println("Option not handled in this if statement.");
         }
 
         /**
-         * 
+         * An if statement with a single println, delaration or call can be written without brackets.
+         * This is the same when using a for loop.
          */
+        if(num == 10)
+            System.out.println("The number 10.");
 
+        /**
+         * Switch statements are structured differently.
+         * The switch takes in a value (num) and then we add cases for it to switch between depending on the value taken.
+         * Since we passed num in the switch case, if our num is 10 then we print "The number 10."
+         */
         switch (num) {
             case 10:
                 System.out.println("The number 10.");
--- src/Programming1/LoopsAndArrays.java
+++ src/Programming1/LoopsAndArrays.java
@@ -8,13 +8,27 @@ package Programming1;
 public class LoopsAndArrays {
     public static void main(String[] args) {
         /**
-         * 
+         * Declaring and defining arrays.
+         * These are arrays, notice the structure here. The same as defining an int or String but we add [].
+         * Since we are creating an array we have to treat it as such. An array is nothing more than a set of items.
+         * For ints we list ints as a set of numbers, ie {1, 2, 3, 4, 5}.
+         * For Strings we list them as a set of Strings, ie {"Hello", "World"}.
          */
         int nums[] = {1, 2, 3, 4, 5};
-        String[] names = {"William", "Chelsea", "Dailen", "Hayron", "Dylan"};
+        String[] names = {"William", "Chelsea", "Dailen", "Hayron", "Mark"};
 
         /**
+         * Printing arrays are a bit different from printing regular variables.
+         * Since arrays are sets, we have to loop through them to get the values.
+         * To get the values of an array we write the array name with [i]. The i will get the index or position in the array and give the value.
+         * Arrays start at 0 in Java. 0 being the first index of the array.
+         * So [1, 2, 3, 4, 5] is our array.
+         *    [0, 1, 2, 3, 4] are the indexes or positions of our numbers in the array.
+         * If we write System.out.println(nums[2]); we will print the number 3 since 3 is in the 2nd index of the array.
          * 
+         * We need a counter to loop through the array. We can use an int called i and start at the first position.
+         * Then we want to loop through the array until the end, so i < nums.length; in our example, nums.length = 5. - Amount of items in the array.
+         * Then we need to increment (add one in this example) our i. We do this by writting i++ which will add 1 to i after an iteration of the loop.
          */
         System.out.println("Printing nums with for i loop.");
         for (int i = 0; i < nums.length; i++) {
@@ -22,7 +36,11 @@ public class LoopsAndArrays {
         }
 
         /**
-         * 
+         * This is called an "enhanced for loop".
+         * Notice the difference in structure.
+         * We create an int called num (could use i here too)
+         * Then we loop through the array.
+         * Think of this as for every num in our nums array we want to print that num.
          */
         System.out.println("Printing nums with foreach loop.");
         for (int num : nums) {
@@ -30,7 +48,7 @@ public class LoopsAndArrays {
         }
 
         /**
-         * 
+         * Regular and enhanced for loop but printing Strings from the string array.
          */
         System.out.println("Printing names with for i loop.");
         for (int i = 0; i < names.length; i++) {
@@ -40,5 +58,13 @@ public class LoopsAndArrays {
         for (String name : names) {
             System.out.println(name);
         }
+
+        /**
+         * Since we touched on having single line if statements without brackets, for loops can do that too.
+         * Same can be done for the String array.
+         */
+        System.out.println("Printing nums with for i loop.");
+        for (int i = 0; i < names.length; i++) 
+            System.out.println(nums[i]);
     }
 }
